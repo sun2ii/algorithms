2be8bbb26abc923d0e826b39681e97be58859eb2
Ben Basuni <benbasuni1@gmail.com>
2020-10-30 12:50:44 -0400

    added reverse elements logging

--- interview_camp/1_2pointers/1_reverseElements.js
+++ interview_camp/1_2pointers/1_reverseElements.js
@@ -1,3 +1,15 @@
+/*
+-------------------------------------
+        Reverse Array
+-------------------------------------
+
+1. >< swap
+-------------------------------------
+| 0 5 | [1, 2, 3, 4, 5, 6] 
+| 1 4 | [6, 2, 3, 4, 5, 1] 
+| 2 3 | [6, 5, 3, 4, 2, 1] 
+*/
+
 const { arrayHelper: utils, log, line } = require('../../__common/utils');
 
 // Sorted -> 2 Pointer
@@ -7,7 +19,10 @@ const reverseArray = a => {
 	let i = 0;
 	let j = a.length - 1;
 	
-	log('\nMost basic 2 pointer swap');
+	line();
+	log('\tReverse Array');
+	line();
+	log('\n1. >< swap');
 	line();
 	while (i < j) {
 		utils.prettyPrint(a, i, j);
